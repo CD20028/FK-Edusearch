199bb02484527a15e80fe1c56549384e33a0fb06
samimosky <136496587+samimosky@users.noreply.github.com>
2023-06-16 04:52:44 +0800

    fs

---
+++ Complaint Database Connection.php
@@ -0,0 +1,14 @@
+<?php
+// Database configuration
+$host = "localhost";
+$username = "samimosky";
+$password = "Syaa271811#";
+$database = "fk-edu";
+
+// Create database connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
