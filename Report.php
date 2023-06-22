07d784d6d2605256162ac18a50f9eb737af9e355
samimosky <136496587+samimosky@users.noreply.github.com>
2023-06-23 01:14:52 +0800

    gf

---
+++ Report.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>FK-Edusearch Reporting and Analytics</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <h1>FK-Edusearch</h1>
+  <table id="report-table">
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Report ID</th>
+        <th>User ID</th>
+        <th>Title</th>
+        <th>Type</th>
+        <th>Description</th>
+        <th>Report Process</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+  
+  
+      <div class="topnav">
+        <a href="#manageC">Manage Complaints</a>
+        <a class="active" href="#manageR">Manage Report</a>
+        <a href="#manageE">Manage Experts</a>
+        <a href="#manageR">Manage Role</a>
+        <a href="#manageU">Manage User</a>
+      </div>
+      
+      <div style="padding-right:16px">
+        <h2>Manage Reports</h2>
+        <tr>
+          <th>Date</th>
+          <th>Report ID</th>
+        </tr>
+      </div>
+
+    </tbody>
+  </table>
+  
+  <script src="scripts.js"></script>
+</body>
+</html>
